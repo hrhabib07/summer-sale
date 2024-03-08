5e079264ee571cf60f10755fa755132d9e752461
Hr Habib <mdhabibur.hr7@gmail.com>
2024-03-08 17:21:23 +0600

    new bug solved

--- script/prod1.js
+++ script/prod1.js
@@ -86,17 +86,3 @@ document.getElementById('product-6').addEventListener('click', function(){
     totalPrice = parseFloat(totalPrice);
     document.getElementById('total-price').innerText = productPrice6 + totalPrice;
 });
-// product-6 
-document.getElementById('product-6').addEventListener('click', function(){
-    let productPrice6 = document.getElementById('product-6-price').innerText;
-    productPrice6 = parseFloat(productPrice6);
-    let productTitle6 = document.getElementById('product-6-title').innerText;
-    console.log(productPrice6, productTitle6);
-    const olContainer =  document.getElementById('ol-container');
-    let newItem = document.createElement('li');
-    newItem.innerText = productTitle6;
-    olContainer.appendChild(newItem);
-    let totalPrice = document.getElementById('total-price').innerText;
-    totalPrice = parseFloat(totalPrice);
-    document.getElementById('total-price').innerText = productPrice6 + totalPrice;
-});
