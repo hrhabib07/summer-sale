d550f8f9b79dbcba8d1469ec29a50dc2924e14af
Hr Habib <mdhabibur.hr7@gmail.com>
2024-03-08 17:17:43 +0600

    product 6 fuction added

--- script/fucntion.js
+++ script/fucntion.js
@@ -1,14 +1,14 @@
-// product-5 
-document.getElementById('product-5').addEventListener('click', function(){
-    let productPrice5 = document.getElementById('product-5-price').innerText;
-    productPrice5 = parseFloat(productPrice5);
-    let productTitle5 = document.getElementById('product-5-title').innerText;
-    console.log(productPrice5, productTitle5);
+// product-6 
+document.getElementById('product-6').addEventListener('click', function(){
+    let productPrice6 = document.getElementById('product-6-price').innerText;
+    productPrice6 = parseFloat(productPrice6);
+    let productTitle6 = document.getElementById('product-6-title').innerText;
+    console.log(productPrice6, productTitle6);
     const olContainer =  document.getElementById('ol-container');
     let newItem = document.createElement('li');
-    newItem.innerText = productTitle5;
+    newItem.innerText = productTitle6;
     olContainer.appendChild(newItem);
     let totalPrice = document.getElementById('total-price').innerText;
     totalPrice = parseFloat(totalPrice);
-    document.getElementById('total-price').innerText = productPrice5 + totalPrice;
+    document.getElementById('total-price').innerText = productPrice6 + totalPrice;
 });
--- script/prod1.js
+++ script/prod1.js
@@ -71,3 +71,18 @@ document.getElementById('product-5').addEventListener('click', function(){
     totalPrice = parseFloat(totalPrice);
     document.getElementById('total-price').innerText = productPrice5 + totalPrice;
 });
+
+// product-6 
+document.getElementById('product-6').addEventListener('click', function(){
+    let productPrice6 = document.getElementById('product-6-price').innerText;
+    productPrice6 = parseFloat(productPrice6);
+    let productTitle6 = document.getElementById('product-6-title').innerText;
+    console.log(productPrice6, productTitle6);
+    const olContainer =  document.getElementById('ol-container');
+    let newItem = document.createElement('li');
+    newItem.innerText = productTitle6;
+    olContainer.appendChild(newItem);
+    let totalPrice = document.getElementById('total-price').innerText;
+    totalPrice = parseFloat(totalPrice);
+    document.getElementById('total-price').innerText = productPrice6 + totalPrice;
+});
